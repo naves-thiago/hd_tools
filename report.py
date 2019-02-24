0ecde31d1c3936f1b83554aad0c4cb59f380d269
Thiago Naves <naves.thiago@gmail.com>
2019-02-24 01:11:13 -0300

    Fix codding style

--- report.py
+++ report.py
@@ -10,10 +10,10 @@ UNKNOWN = 3
 positions = []
 
 def check_drive(dev, raw_limits):
-    ''' dev = "/dev/ada0"
+    ''' dev = '/dev/ada0'
         raw_limits = {attribute id:[WARN threshold, BAD threshold], 5:[0, 0], 10:[50, 100], ...}
         output = {attribute id:{status:GOOD | WARN | BAD,
-                                name:"Attribute name",
+                                name:'Attribute name',
                                 raw_value:SMART value},
                   attribute id:{...}
                  }'''
@@ -23,52 +23,52 @@ def check_drive(dev, raw_limits):
 
     for attr in raw_limits:
         if attr in smart:
-            raw = int(smart[attr]["raw"])
-            out[attr] = {"name":smart[attr]["name"], "value":smart[attr]["raw"]}
-            if smart[attr]["when_failed"] != "-":
+            raw = int(smart[attr]['raw'])
+            out[attr] = {'name':smart[attr]['name'], 'value':smart[attr]['raw']}
+            if smart[attr]['when_failed'] != '-':
                 # Drive reported failure
-                out[attr]["status"] = BAD
+                out[attr]['status'] = BAD
             elif raw > raw_limits[attr][1]:
                 # Above higher threshold
-                out[attr]["status"] = BAD
+                out[attr]['status'] = BAD
             elif raw > raw_limits[attr][0]:
                 # Above lower threshold
-                out[attr]["status"] = WARN
+                out[attr]['status'] = WARN
             else:
-                out[attr]["status"] = GOOD
+                out[attr]['status'] = GOOD
         else:
             out[attr] = {}
 
     return out
 
 def tput(*params):
-    tmp = ["tput"]
+    tmp = ['tput']
     tmp.extend(params)
-    return subprocess.Popen(tmp, stdout=subprocess.PIPE).stdout.read().decode("utf-8")
+    return subprocess.Popen(tmp, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 
-term_val_spacing   = "    "
-term_val_border    = "  "
+term_val_spacing   = '    '
+term_val_border    = '  '
 
 # Hardcoded values because I can't get tput to work properly in FreeNAS
-term_color_bad     = "\x1b[30m" + "\x1b[41m"
-term_color_warning = "\x1b[30m" + "\x1b[43m"
-term_color_good    = "\x1b[30m" + "\x1b[42m"
-term_color_unknown = "\x1b[30m" + "\x1b[44m"
-term_color_default = "\x1b(B\x1b[m"
+term_color_bad     = '\x1b[30m' + '\x1b[41m'
+term_color_warning = '\x1b[30m' + '\x1b[43m'
+term_color_good    = '\x1b[30m' + '\x1b[42m'
+term_color_unknown = '\x1b[30m' + '\x1b[44m'
+term_color_default = '\x1b(B\x1b[m'
 
-#term_color_bad     = tput("setaf", "0") + tput("setab", "1") # Black on Red
-#term_color_warning = tput("setaf", "0") + tput("setab", "3") # Black on Yellow
-#term_color_good    = tput("setaf", "0") + tput("setab", "2") # Black on Green
-#term_color_unknown = tput("setaf", "0") + tput("setab", "4") # Black on Green
-#term_color_default = tput("sgr0")                            # Default colors
+#term_color_bad     = tput('setaf', '0') + tput('setab', '1') # Black on Red
+#term_color_warning = tput('setaf', '0') + tput('setab', '3') # Black on Yellow
+#term_color_good    = tput('setaf', '0') + tput('setab', '2') # Black on Green
+#term_color_unknown = tput('setaf', '0') + tput('setab', '4') # Black on Green
+#term_color_default = tput('sgr0')                            # Default colors
 
 def format_val(name, val, color):
     colors = {GOOD:term_color_good, WARN:term_color_warning, BAD:term_color_bad,
               UNKNOWN:term_color_unknown}
-    return colors[color] + term_val_border + name + ": " + val + term_val_border + term_color_default
+    return colors[color] + term_val_border + name + ': ' + val + term_val_border + term_color_default
 
 def print_smart_per_drive(smart, columns = 1, attribute_order = None, hdd_order = None):
-    ''' smart = {"hdd_name":{smart values}, "hdd_name":{smart values}, ...}
+    ''' smart = {'hdd_name':{smart values}, 'hdd_name':{smart values}, ...}
         columns = SMART attributes per line.
         attribute_order = Optional list of smart attribute ids.
         The smart values will be printed in this order.
@@ -83,30 +83,30 @@ def print_smart_per_drive(smart, columns = 1, attribute_order = None, hdd_order 
         # Sort attributes by name
         hd0 = smart[next(iter(smart))]
         def sort_key(v):
-            return hd0[v]["name"]
+            return hd0[v]['name']
         attribute_order = list(hd0)
         attribute_order.sort(key = sort_key)
 
     for name in hdd_order:
         print(name)
-        line = ""
+        line = ''
         count = 0
         for val in attribute_order:
             attr = smart[name][val]
-            line += format_val(attr["name"], attr["value"], attr["status"])
+            line += format_val(attr['name'], attr['value'], attr['status'])
             count += 1
             if count % columns == 0 or count == len(attribute_order):
                 print(line)
-                line = ""
+                line = ''
             else:
                 line += term_val_spacing
-        if line != "":
+        if line != '':
             print(line)
 
-        print("")
+        print('')
 
 def print_smart_per_attribute(smart, columns = 1, attribute_order = None, hdd_order = None):
-    ''' smart = {"hdd_name":{smart values}, "hdd_name":{smart values}, ...}
+    ''' smart = {'hdd_name':{smart values}, 'hdd_name':{smart values}, ...}
         columns = Drives per line.
         attribute_order = Optional list of smart attribute ids.
         The smart values will be printed in this order.
@@ -121,39 +121,39 @@ def print_smart_per_attribute(smart, columns = 1, attribute_order = None, hdd_or
     if not attribute_order:
         # Sort attributes by name
         def sort_key(v):
-            return hd0[v]["name"]
+            return hd0[v]['name']
         attribute_order = list(hd0)
         attribute_order.sort(key = sort_key)
 
     for val in attribute_order:
         attr = hd0[val]
-        print(attr["name"])
-        line = ""
+        print(attr['name'])
+        line = ''
         count = 0
         for name in hdd_order:
             attr = smart[name].get(val)
             if attr:
-                line += format_val(name, attr["value"], attr["status"])
+                line += format_val(name, attr['value'], attr['status'])
             else:
-                line += format_val(name, "???", UNKNOWN)
+                line += format_val(name, '???', UNKNOWN)
 
             count += 1
             if count % columns == 0 or count == len(attribute_order):
                 print(line)
-                line = ""
+                line = ''
             else:
                 line += term_val_spacing
-        if line != "":
+        if line != '':
             print(line)
 
-        print("")
+        print('')
 
-parser = ArgumentParser(description="Check drives' SMART attributes against \"warning\" and \"bad\" thresholds. "
-                                    "Output color coded in green, yellow or red representing a good / warning / bad value.")
-parser.add_argument("disks", metavar="disk", type=str, nargs="+", help="Disks to check")
-parser.add_argument("-d", "--perdrive", action="store_true", dest="perdrive",
-                    help="Group output per drive instead of per SMART attribute")
-parser.add_argument("-c", "--columns", metavar="N", type=int, default=1, help="Output column count (default 1)")
+parser = ArgumentParser(description='Check drives\' SMART attributes against "warning" and "bad" thresholds. '
+                                    'Output color coded in green, yellow or red representing a good / warning / bad value.')
+parser.add_argument('disks', metavar='disk', type=str, nargs='+', help='Disks to check')
+parser.add_argument('-d', '--perdrive', action='store_true', dest='perdrive',
+                    help='Group output per drive instead of per SMART attribute')
+parser.add_argument('-c', '--columns', metavar='N', type=int, default=1, help='Output column count (default 1)')
 opts = parser.parse_args()
 
 limits = {4:[1000, 2000],
@@ -162,7 +162,7 @@ limits = {4:[1000, 2000],
           187:[0,5],
           194:[35, 41],
           197:[0,3],
-          198:[0,3]
+          198:[0,3],
 }
 smart = {}
 for dev in opts.disks:
