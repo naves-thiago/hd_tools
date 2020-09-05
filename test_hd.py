cb32dfc1d82ffbfb997cce9f8cb3d126397196f1
Thiago Naves <naves.thiago@gmail.com>
2020-09-05 16:33:59 -0300

    Catch and report errors. Report progress

--- test_hd.py
+++ test_hd.py
@@ -1,32 +1,74 @@
 from random import randint
 import sys
-print('This will write to ' + sys.argv[1])
+
+sector_size = 512
+write_count = 10000
+
+f = open(sys.argv[1], 'rb')
+e = f.seek(0, 2)
+f.close()
+print('This will write to %s (size: %d B [%.2f TB])' % (sys.argv[1], e, e / (1000**4)))
 if input('OK? [y, N] ') != 'y':
     exit()
 
 print('Running...')
 f = open(sys.argv[1], 'r+b')
 e = f.seek(0, 2)
-e /= 4096
+e /= sector_size
 e -= 1
 
 data = []
-for i in range(4096):
-    data.append(randint(0,255))
+for i in range(sector_size):
+    data.append(randint(0, 255))
 data = bytes(data)
 
+print("Writting...")
 secs=[]
-for i in range(10000):
+for i in range(write_count):
     p = randint(0, e)
     secs.append(p)
-    f.seek(p * 4096)
-    f.write(data)
-    f.flush()
+    try:
+        f.seek(p * sector_size)
+    except Exception as e:
+        print('Seek %d failed with exeption: %s' % (p * sector_size, str(e)))
+        break
 
-for s in secs:
-    f.seek(s * 4096)
-    if f.read(4096) != data:
-        print(f'Error sector {s}')
+    try:
+        f.write(data)
+    except Exception as e:
+        print('Write %d failed with exeption: %s' % (p * sector_size, str(e)))
+        break
 
+    try:
+        f.flush()
+    except Exception as e:
+        print('Flush %d failed with exeption: %s' % (p * sector_size, str(e)))
+        break
+
+    if i % (write_count / 10) == 0:
+        print('%d/%d' % (int(i / (write_count / 10)), 10))
+
+print('10/10')
+print("Reading...")
+read_count = len(secs)
+for i in range(read_count):
+    s = secs[i]
+    try:
+        f.seek(s * sector_size)
+    except Exception as e:
+        print('Seek %d failed with exeption: %s' % (s * sector_size, str(e)))
+        break
+
+    try:
+        if f.read(sector_size) != data:
+            print(f'Error sector {s}')
+    except Exception as e:
+        print('Read %d failed with exeption: %s' % (s * sector_size, str(e)))
+        break
+
+    if i % (read_count / 10) == 0:
+        print('%d/%d' % (int(i / (read_count / 10)), 10))
+
+print('10/10')
 f.close()
 print('Done')
